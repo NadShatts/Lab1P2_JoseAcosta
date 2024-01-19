ed5d1fc6104342e633c9aa916cd552b309f43587
NadShatts <josedaniel202006@gmail.com>
2024-01-19 13:43:25 -0600

    Segunda entrega

--- Lab1P2_JoseAcosta/src/lab1p2_joseacosta/Lab1P2_JoseAcosta.java
+++ Lab1P2_JoseAcosta/src/lab1p2_joseacosta/Lab1P2_JoseAcosta.java
@@ -12,49 +12,81 @@ import java.util.Scanner;
  * @author josed
  */
 public class Lab1P2_JoseAcosta {
-    
+
+    private static ArrayList<Usuario> usuarios = new ArrayList<>();
+    private static final String[] ACCEPTED_DOMAINS = {"gmail.com", "outlook.com", "yahoo.com", "icloud.com", "protonmail.com", "fastmail.com"};
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
         Scanner entrada = new Scanner(System.in);
-        
+
         int opcion;
-        do{
+        do {
             System.out.println("Menu:");
             System.out.println("1. Registrar Usuario");
             System.out.println("2. Listar Todo");
             System.out.println("3. Listar por Dominio.");
             System.out.println("4. Salir");
+            System.out.print("Ingrese su opcion: ");
             opcion = entrada.nextInt();
-            
-            switch(opcion){
-                
+
+            switch (opcion) {
+
                 case 1:
-                    System.out.println("Bienvenido al registro del usuario....");
-                   break;
-                   
+                    registroUsuario();
+                    break;
+
                 case 2:
                     System.out.println("Su lista seria la siguiente: ");
+                    listarTodo();
                     break;
-                    
+
                 case 3:
-                  break;
-                  
+                    break;
+
                 case 4:
                     System.out.println("Saliendo del Registro. Vuelva Pronto..");
                     break;
-                   
+
                 default:
                     System.out.println("Opcion invalida para el registro. Intente denuevo.");
-                   
-                
+
             }
-            
-        }while(opcion!=4);
+
+        } while (opcion != 4);
+    }
+
+    private static void registroUsuario() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la información del usuario:");
+        System.out.print("Nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.print("Apellido: ");
+        String apellido = entrada.nextLine();
+        System.out.print("Fecha de Nacimiento (AAAA-MM-DD): ");
+        String fechaNacimiento = entrada.nextLine();
+        System.out.print("Correo Electrónico: ");
+        String correoElectronico = entrada.nextLine();
+        System.out.print("Contraseña: ");
+        String contraseña = entrada.nextLine();
+
+        
+        Usuario usuario = new Usuario(nombre, apellido, fechaNacimiento, correoElectronico, contraseña);
+        usuarios.add(usuario);
+        System.out.println("Usuario registrado exitosamente.");
+
     }
     
-}
+    
+    private static void listarTodo(){
+        
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario.toString());
+        }
+    }
+    }
+    
+
---
+++ Lab1P2_JoseAcosta/src/lab1p2_joseacosta/Usuario.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1p2_joseacosta;
+
+/**
+ *
+ * @author josed
+ */
+public class Usuario {
+    
+    
+    private String nombre;
+    private String apellido;
+    private String nacimiento;
+    private String correo;
+    private String contraseña;
+
+    public Usuario(String nombre, String apellido, String nacimiento, String correo, String contraseña) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacimiento = nacimiento;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+    
+    
+    
+    
+}
